b9c20d593d924d1908a517e93e077be04a64a70d
Sam Ly <sam@samly.me>
2023-04-01 18:36:41 -0700

    complete 1.3-1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,55 +1,56 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course}/>
-      <Content 
-        part1={part1}
-        exercises1={exercises1}
-        part2={part2}
-        exercises2={exercises2}
-        part3={part3}
-        exercises3={exercises3}
-      />
-
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
   )
 }
 
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
   )
 }
 
 const Content = (props) => {
   return (
-    <div>
-      <Part part={props.part1} exercise={props.exercises1}/>
-      <Part part={props.part2} exercise={props.exercises2}/>
-      <Part part={props.part3} exercise={props.exercises3}/>
-    </div>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercise}</p>
+    props.parts.map(
+      (item, index) => {
+        return <p key={index}>{item.name} {item.exercises}</p>
+      }
+    )
   )
 }
 
 const Total = (props) => {
+  let total = 0
+  props.parts.forEach(
+    item => {
+      total += item.exercises
+    }
+  )
   return (
-    <p>Number of exercises {props.total}</p>
+    <p>Number of exercises {total}</p>
   )
 }
-
 export default App
